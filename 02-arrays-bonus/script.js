49242edfe2a4c6562654d39b91a60e468d802b37
donatimar <donatimar@gmail.com>
2024-10-16 15:06:33 +0200

    02 - 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,6 +10,12 @@ console.log("01 - Insegnanti in ordine invertito:", reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+console.log("02 - Nomi contenenti almeno 5 lettere:", longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
