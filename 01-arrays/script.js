f86cd14241d1bdd708f56b9617f23659a2b042f3
donatimar <donatimar@gmail.com>
2024-10-16 14:22:59 +0200

    02 - 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -3,9 +3,11 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log("Fourth Teacher:", fourthTeacher);
+console.log("Quarto insegnante:", fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers.splice(4, 1, "Patrick");
+console.log("Insegnante che sostituisce il quinto nell'elenco", teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
